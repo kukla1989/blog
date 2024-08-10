51f5670020e0a0b965565d283ecb1bd5423005a7
kukla <kukla.1989@ukr.net>
2024-08-10 13:25:11 +0300

    add responsive version of the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.scss';
-import Header from './components/Header/Header';
+import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      <div className="container">
+        <Header />
+      </div>
     </div>
   );
 }
